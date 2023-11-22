8b18a7937d5ded7aa25eb7c7c074ee8fcfa0a455
Пользователь <world.worensi@gmail.com>
2023-11-22 19:45:51 +0600

    first commit

---
+++ src/Pages/HomePages.js
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from "react";
+import Header from "../components/Header";
+import axios from "axios";
+import MealList from "../components/MealList";
+import Footer from "../components/Footer";
+const HomePages = ()=>{
+const[meals,setMeals]=useState([])
+
+    useEffect(() => {
+        axios('https://www.themealdb.com/api/json/v2/1/randomselection.php')
+            .then(({data})=>setMeals(data.meals))
+    }, []);
+
+    return(
+
+        <div>
+            <Header/>
+            <div className={'container'}>
+                <h1>Главное меню</h1>
+                <MealList meals={meals}/>
+            </div>
+            <Footer/>
+        </div>
+    )
+}
+export default HomePages
